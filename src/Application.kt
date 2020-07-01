719304731de456767319588fca0a735583898e50
Braedon <braedongough@gmail.com>
2020-07-01 21:16:04 +0200

    Start lesson 7

--- src/Application.kt
+++ src/Application.kt
@@ -1,7 +1,13 @@
 package com.raywenderlich
 
+import com.raywenderlich.api.*
+import com.raywenderlich.repository.*
 import com.raywenderlich.webapp.*
 import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.gson.*
+import io.ktor.http.*
+import io.ktor.response.*
 import io.ktor.routing.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
@@ -9,9 +15,34 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+
+    install(DefaultHeaders)
+
+    install(StatusPages) {
+        exception<Throwable> { e ->
+            call.respondText(e.localizedMessage, ContentType.Text.Plain, HttpStatusCode.InternalServerError)
+        }
+    }
+
+    install(ContentNegotiation) {
+        gson {
+            setPrettyPrinting()
+        }
+    }
+
+    val db = InMemoryRepository()
+
     routing {
         home()
+        about()
+
+        //API
+        phrase(db)
     }
+
+
 }
 
-// NEXT LESSON IS LESSON 4
+// Next up, start lesson 7
+
+const val API_VERSION = "/api/v1"
---
+++ src/model/Request.kt
@@ -0,0 +1,3 @@
+package com.raywenderlich.model
+
+data class Request(val emoji: String, val phrase: String)
